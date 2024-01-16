007075969b76934448f200952d74843cafafe7bc
Michał Kolczak <michal.kolczak123@gmail.com>
2024-01-16 11:22:10 +0100

    Bug fixes

--- include/DataGenerator.hpp
+++ include/DataGenerator.hpp
@@ -1,15 +1,16 @@
 #ifndef DATAGENERATOR_HPP
 #define DATAGENERATOR_HPP
 
+#include <cstddef>
 #include <vector>
 
 class DataGenerator {
-private:
-    size_t vector_size{0};
-
 public:
     DataGenerator(size_t size);
     std::vector<int> generate_vector();
+
+private:
+    const size_t vector_size;
 };
 
 #endif
